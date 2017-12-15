6436dcb9a47d8c6c18f755e3ae0318b40d5abb01
Last_GuardiaN <tonyzhao1314@gmail.com>
2017-12-15 20:00:38 +0800

    day 2 p2

---
+++ ex21.py
@@ -0,0 +1,16 @@
+people = 10
+candy = 20
+hats = 5
+
+if people < candy:
+	print("everyone can have a candy by the end!")
+
+if people > candy:
+	print("there is not enough candy for everyone!")
+
+if people < hats:
+	print("everyone can have a hat by the end!")
+
+if people > hats:
+	print("there is not enough hat for everyone!")
+	
